3235858b972acee741fa7b84c15f7ca6a4a67735
caser <itscaseyr@gmail.com>
2019-10-23 15:34:50 -0400

    Moved static to project/statics, fixed css

--- prism_project/prism_project/settings.py
+++ prism_project/prism_project/settings.py
@@ -120,7 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'statics'),
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
