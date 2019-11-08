6c3712c9f1f785d6c1410f25bcba8b1f4f5801ae
caser <itscaseyr@gmail.com>
2019-11-07 21:27:35 -0600

    Added page model to database

--- prism_project/pages/models.py
+++ prism_project/pages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Page(models.Model):
+    title = models.CharField(max_length=128)
+    content = models.TextField()
+    date_modified = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete="[USER_DELETED]")
+
