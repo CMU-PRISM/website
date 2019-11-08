82426a288e899162a02304cc75c6593238e9360f
caser <itscaseyr@gmail.com>
2019-11-08 15:30:35 -0600

    Change pages/ loading to use database data instead of dummy data

--- prism_project/pages/views.py
+++ prism_project/pages/views.py
@@ -1,37 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Page
 
 # Add views here. Add by adding new function def subpage(request): return render(request, html file path, context)
 # Add html not in a file using HttpResponse(html)
 
-#dummy code to test things
-pages = [
-    {
-        'last_author': 'caser',
-        'name': 'homepage',
-        'date_created': '2019.9.26',
-        'date_last_modified': '2019.9.26'
-    },
-
-    {
-        'last_author': 'faker',
-        'name': 'homepage???? [not clickbait]',
-        'date_created': '2019.9.27',
-        'date_last_modified': '2019.10.3'
-    }
-]
-
 # Take user requests to ./functionName
-def home(request):
+def home(request): # ./pages/
     context = {
-        'pages': pages
+        'pages': Page.objects.all()
     }
     return render(request, 'pages/home.html', context)
 
-def about(request):
+def about(request): # ./pages/about
     return render(request, 'pages/about.html', {'title':'About'})
 
-def resources(request):
+def resources(request): # ./pages/resources
     return HttpResponse('resources page') #TODO: replace the text here with the proper html file
 
 def officeHours(request):
