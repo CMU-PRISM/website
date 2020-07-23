dfcb69a0acf8a57a1fb26d35286a20bf297bf7c6
Casey Rodriguez <itscaseyr@gmail.com>
2020-07-23 13:15:17 -0400

    Reworked page models.py for dynamic routing w/ slugs

--- prism_project/pages/models.py
+++ prism_project/pages/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.template.defaultfilters import slugify
 from markdownx.models import MarkdownxField
 from markdownx.utils import markdownify
 from prism_project import settings
@@ -8,16 +9,20 @@ def get_sentinel_user(): # sets the user 'deleted' as the author of a post, shou
     return None#User.objects.get_or_create(username='deleted')[0]
 
 class Page(models.Model):
-    title = models.CharField(max_length=128) # title of page
-    slug = 'page:' + str(title).replace(' ', '_') # change spaces in title to underscores and create end of url
+    title = models.CharField(max_length=128, unique=True) # title of page
+    slug_txt = str(title).replace(' ', '-').lower() # change spaces in title to underscores and create end of url
+    slug = slugify(slug_txt)
     content = MarkdownxField() # interpret Markdownx to page
     date_modified = models.DateTimeField(default=timezone.now) # only keep the date and time of the most recent page update
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET(get_sentinel_user()), default=None) # page should show who edited it and display "deleted" if the user has since been deleted
-    link = "{% url clean_title %}" # link to any page should juse be prism.andrew.cmu.edu/pages/TITLE
     desc = models.TextField(default="default description") # short description of page for directory
     
     def __str__(self): # give string representation of the page object to be "title by author"
-        return "%s by %s" % (self.title, self.author)
+        return "%s \n \n %s" % (self.title, self.desc)
     
     def formatted_markdown(self):
         return markdownify(self.content)
+    
+    def get_slug(self):
+        return self.slug
+
