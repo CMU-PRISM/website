933ed6f9db35a4c444f78710b82a9a102c025de6
Casey Rodriguez <itscaseyr@gmail.com>
2020-08-18 18:08:09 -0400

    Prep updates for dynamic generation
    Similar to pages, updates should be user-friendly to create. Work needs to be done to make for dynamic update display in a directory for updates.

--- prism_project/updates/models.py
+++ prism_project/updates/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from django.template.defaultfilters import slugify
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdownify
+from prism_project import settings
 
-# Create your models here.
+class Update(models.Model):
+    title = models.CharField(max_length=64, unique=True) # title of page
+    content = MarkdownxField(max_length=512) # interpret Markdownx to page
+    date_modified = models.DateTimeField(default=timezone.now) # only keep the date and time of the most recent page update
+    desc = models.TextField(default="default description", max_length=256) # short description of page for directory
+    
+    def __str__(self): # give string representation of the page object to be title and description
+        return "%s %s %s %s %s" % (self.title, self.content, self.date_modified, self.desc)
+    
+    def __eq__(self, other):
+        return self.title == other.title and self.content == other.content and self.date_modified == other.date_modified and self.desc == other.desc
+    
+    def formatted_markdown(self):
+        return markdownify(self.content)
+    
+    def slug_as_text(self):
+        return str(self.title).replace(' ', '-').lower()
