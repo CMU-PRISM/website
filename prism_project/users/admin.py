b1ef30e65b4dafa696fbf81f371a7967cb30809a
Casey Rodriguez <itscaseyr@gmail.com>
2020-09-01 12:17:02 -0400

    Staff can no longer create superuser users

--- prism_project/users/admin.py
+++ prism_project/users/admin.py
@@ -1,14 +1,51 @@
+from typing import Set
+
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
     model = CustomUser
     list_display = ['email', 'username',]
+    readonly_fields = [
+        'date_joined'
+    ]
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        disabled_fields = set()  # type: Set[str]
+
+        if not is_superuser:
+            disabled_fields |= {
+                'username',
+                'is_superuser',
+                'is_staff',
+                'user_permissions',
+            }
+
+        # Prevent non-superusers from editing their own permissions
+        if (
+            not is_superuser
+            and obj is not None
+            and obj == request.user
+        ):
+            disabled_fields |= {
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            }
+
+        for f in disabled_fields:
+            if f in form.base_fields:
+                form.base_fields[f].disabled = True
+
+        return form
 
 admin.site.register(CustomUser, CustomUserAdmin)
