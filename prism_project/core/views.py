befeb4a659dcab8a5c3c977bdc2e38ca05783769
Casey Rodriguez <itscaseyr@gmail.com>
2020-12-15 12:48:54 -0500

    Dynamic image on index

--- prism_project/core/views.py
+++ prism_project/core/views.py
@@ -1,4 +1,5 @@
 from updates.models import Update
+from .models import Door
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,6 +7,7 @@ from django.shortcuts import render
 def index(request): # homepage
     # context being the most recent update
     context = {
-        'update': Update.objects.order_by('-date_modified')[0]
+        'update': Update.objects.order_by('-date_modified')[0],
+        'door': Door.objects.all()[0]
     }
     return render(request, 'core/index.html', context)
