53d2cf8213880d0091c5a81fa6c1959043ecd798
Casey Rodriguez <itscaseyr@gmail.com>
2021-04-21 02:14:32 -0400

    Debug is False in Production

--- prism_project/prism_project/settings.py
+++ prism_project/prism_project/settings.py
@@ -12,7 +12,7 @@ with open(os.path.join(BASE_DIR, 'secretkey.txt')) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # Django needs to know what the server can be hosted on. For now, this will be the final website, the local machine, and a wild card, for debug purposes only.
 ALLOWED_HOSTS = ['prism.andrew.cmu.edu', 'prism-02.club.cc.cmu.edu', 'localhost', "*" if DEBUG else ""]
 
