06eaeda99d4dcb2cfc0c21867a3752c905f8419a
Casey Rodriguez <itscaseyr@gmail.com>
2022-07-12 02:48:30 -0400

    Removed the extra <hr> at the end of the page and update directory lists

--- prism_project/pages/views.py
+++ prism_project/pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Page
+from django.apps import apps
+
+# Load model dynamically to avoid circular import issues
+PageModel = apps.get_model('pages', "Page")
 
 # Add views here. Add by adding new function def subpage(request): return render(request, html file path, context)
 # Add html not in a file using HttpResponse(html)
@@ -8,16 +10,14 @@ from .models import Page
 # Take user requests to ./functionName
 def index(request): # ./pages/
     context = {
-        'pages': Page.objects.all()
+        'pages': PageModel.objects.order_by('-date_modified'),
+        'finalPage': PageModel.objects.order_by('-date_modified').last()
     }
     return render(request, 'pages/index.html', context)
 
-def page_viewer(request, title="404"):
-    # default to page's 404
-    if title == "404":
-        return render(request, 'pages/404.html')
+def page_viewer(request, title):
     context = {
-        'page': Page.objects.get(title=title.lower())
+        'page': PageModel.objects.get(title=title.lower())
     }
     #return render(request, 'pages/x.html', context)
     return render(request, 'pages/page-view.html', context)
--- prism_project/updates/views.py
+++ prism_project/updates/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.apps import apps
 
+# Load model dynamically to avoid circular import issues
 UpdateModel = apps.get_model('updates', "Update")
 
 # Create your views here.
@@ -8,8 +9,8 @@ UpdateModel = apps.get_model('updates', "Update")
 # Take user requests to ./functionName
 def index(request): # ./updates/
     context = {
-        
-        'updates': UpdateModel.objects.order_by('-date_modified')
+        'updates': UpdateModel.objects.order_by('-date_modified'),
+        'finalUpdate': UpdateModel.objects.order_by('-date_modified').last(),
     }
     print(context)
     return render(request, 'updates/index.html', context)
