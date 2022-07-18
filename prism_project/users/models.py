a09b4b4835c8b5e2ff65f824abdf851113929497
Casey Rodriguez <itscaseyr@gmail.com>
2022-07-18 08:39:32 -0400

    Switched to default permission checking

--- prism_project/users/models.py
+++ prism_project/users/models.py
@@ -119,18 +119,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return '{} <{}>'.format(self.get_full_name(), self.email)
     
-    def has_perm(self, perm, obj=None):
-        "Does the user have a specific permission?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    def has_module_perms(self, app_label):
-        "Does the user have permissions to view the app `app_label`?"
-        # Simplest possible answer: Yes, always
-        return True
-    
     class Meta:
         permissions = (
-            ('can_open', "Can mark room as open/closed"),
         )
         
