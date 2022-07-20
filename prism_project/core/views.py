9f810de2e8fe28e0dea234e40e964a248ef5eeca
Casey Rodriguez <itscaseyr@gmail.com>
2022-07-20 04:10:21 -0400

    Changed how doorbot interacts w/ site through POST

--- prism_project/core/views.py
+++ prism_project/core/views.py
@@ -2,14 +2,13 @@ from django.utils import timezone
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.apps import apps
+from django.conf import settings
 
 
 # Load model dynamically to avoid circular import issues
 PageModel = apps.get_model('pages', "Page")
 UpdateModel = apps.get_model('updates', "Update")
 DoorModel = apps.get_model('core', 'Door')
-
-# Create your views here.
 
 def index(request): # homepage
     # context being the most recent update
@@ -23,31 +22,37 @@ def index(request): # homepage
     return render(request, 'core/index.html', context)
 
 def doorOpen(request):
-    if request.method == 'POST' and request.user.has_perm('core.change_door'):
+    # Check if method is POST. If true, then check if user either has permission to change the door of the POST has the right password
+    if request.method == 'POST' and (request.user.has_perm('core.change_door') or request.POST.get('password') == settings.BOT_PASSWORD):
         door = DoorModel.objects.order_by('-date_modified').first()
     
         door.status = 'open'
         door.date_modified = timezone.now()
         door.save()
 
+    # Return user to the page they sent the request from
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 def doorClose(request):
-    if request.method == 'POST' and request.user.has_perm('core.change_door'):
+    # Check if method is POST. If true, then check if user either has permission to change the door of the POST has the right password
+    if request.method == 'POST' and (request.user.has_perm('core.change_door') or request.POST.get('password') == settings.BOT_PASSWORD):
         door = DoorModel.objects.order_by('-date_modified').first()
     
         door.status = 'closed'
         door.date_modified = timezone.now()
         door.save()
 
+    # Return user to the page they sent the request from
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 def doorBusy(request):
-    if request.method == 'POST' and request.user.has_perm('core.change_door'):
+    # Check if method is POST. If true, then check if user either has permission to change the door of the POST has the right password
+    if request.method == 'POST' and (request.user.has_perm('core.change_door') or request.POST.get('password') == settings.BOT_PASSWORD):
         door = DoorModel.objects.order_by('-date_modified').first()
     
         door.status = 'busy'
         door.date_modified = timezone.now()
         door.save()
 
+    # Return user to the page they sent the request from
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
--- prism_project/prism_project/settings.py
+++ prism_project/prism_project/settings.py
@@ -11,6 +11,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 with open(os.path.join(BASE_DIR, 'secretkey.txt')) as f:
     SECRET_KEY = f.read().strip()
 
+with open(os.path.join(BASE_DIR, 'botpassword.txt')) as f:
+    BOT_PASSWORD = f.read().strip()
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 # Django needs to know what the server can be hosted on. For now, this will be the final website, the local machine, and a wild card, for debug purposes only.
@@ -56,7 +59,7 @@ SESSION_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
